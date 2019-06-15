9f35852111b07b576fa162bc229aa459a240c58d
Robin Bisht <robinbisht7@gmail.com>
2019-06-15 20:07:34 +0530

    Single page Application part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,19 +10,29 @@ import { MatButtonModule } from '@angular/material/button';
 
 import 'hammerjs';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishDetailComponent } from './dish-detail/dish-detail.component';
 import {DishService} from './services/dish.service';
 import { from } from 'rxjs';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    DishDetailComponent
+    DishDetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
