43ee9aeb18b9cb4fe126a01059e67fd8926d89a1
Robin Bisht <robinbisht7@gmail.com>
2019-06-16 22:18:50 +0530

    Completing Assingment 2

--- src/app/dish-detail/dish-detail.component.ts
+++ src/app/dish-detail/dish-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {Dish} from '../shared/dish';
+import {DishService} from '../services/dish.service';
+import {Params,ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common'; 
 // import { from } from 'rxjs';
 
 const DISH = {
@@ -51,11 +54,15 @@ const DISH = {
   styleUrls: ['./dish-detail.component.scss']
 })
 export class DishDetailComponent implements OnInit {
-  @Input()
+  
   dish:Dish;
-  constructor() { }
+  constructor(private dishservice:DishService, private route:ActivatedRoute, private location:Location) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.params['id'];
+    this.dish = this.dishservice.getDish(id);
   }
-
+  goBack():void{
+    this.location.back();
+  }
 }
